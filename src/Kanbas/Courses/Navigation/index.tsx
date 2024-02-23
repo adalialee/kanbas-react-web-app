c2e92c6b3b16924d4ad197a900e2273d37ccf6dd
Adalia Lee <adalialee@gmail.com>
2024-02-23 12:13:51 -0500

    fixed paths

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'; // Import React
 import { Link, useLocation } from "react-router-dom";
 import { FaEyeSlash } from "react-icons/fa";
-import "/Users/adalialee/2024/spring/webdev/kanbas-react-web-app/src/Kanbas/Courses/Navigation/index.css";
+import "../Navigation/index.css";
 
 function CourseNavigation() {
     const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People", "Panopto Video", "Discussions", "Announcements", "Pages", "Files"];
