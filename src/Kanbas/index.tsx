9503e062062f21f1372146d5a3af748b938904e3
Adalia Lee <adalialee@gmail.com>
2024-03-31 17:52:29 -0400

    a5 REST fa23

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -19,10 +19,11 @@ import ModifiedNav from "./Navigation/ModifiedNav";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+const API_BASE = process.env.REACT_APP_API_BASE;
 function Kanbas() {
     const { courseId } = useParams();
     const [_courses, setCourses] = useState<any[]>([]);
-    const COURSES_API = "http://localhost:4000/api/courses";
+    const COURSES_API = `${API_BASE}/api/courses`;
     const findAllCourses = async () => {
         const response = await axios.get(COURSES_API);
         setCourses(response.data);
